bb9fe9821b324ff0bacf35594285901a48eb5736
CS <chunsiang10@gmail.com>
2020-11-18 21:25:17 +0800

    another feeble attempt to make things pretty

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -3,8 +3,9 @@ import { MatDatepickerModule } from '@angular/material/datepicker'
 import { MatInputModule } from '@angular/material/input'
 import { MatRadioModule } from '@angular/material/radio'
 import { MatButtonModule } from '@angular/material/button'
+import { MatIconModule } from '@angular/material/icon'
 
-const MATERIAL = [MatDatepickerModule, MatInputModule, MatRadioModule, MatButtonModule ]
+const MATERIAL = [MatDatepickerModule, MatInputModule, MatRadioModule, MatButtonModule, MatIconModule]
 
 @NgModule({
     imports: MATERIAL,
