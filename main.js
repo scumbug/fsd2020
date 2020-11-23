c605f7059091125067d7b5a27e4b20db3b2097b8
CS <chunsiang10@gmail.com>
2020-11-23 16:27:41 +0800

    created api endpoint to pull data

--- main.js
+++ main.js
@@ -5,13 +5,13 @@ require('dotenv').config();
 
 //declare const
 const cart = [
-	{ id: 1, item: 'apple', quantity: 6 },
-	{ id: 2, item: 'pear', quantity: 3 },
-	{ id: 3, item: 'banana', quantity: 66 },
-	{ id: 4, item: 'peach', quantity: 23 },
-	{ id: 5, item: 'watermelon', quantity: 1 },
-	{ id: 6, item: 'pineapple', quantity: 45 },
-	{ id: 7, item: 'orange', quantity: 32 },
+	{ id: 1, name: 'apple', quantity: 6 },
+	{ id: 2, name: 'pear', quantity: 3 },
+	{ id: 3, name: 'banana', quantity: 66 },
+	{ id: 4, name: 'peach', quantity: 23 },
+	{ id: 5, name: 'watermelon', quantity: 1 },
+	{ id: 6, name: 'pineapple', quantity: 45 },
+	{ id: 7, name: 'orange', quantity: 32 },
 ];
 
 //declare env
@@ -40,6 +40,25 @@ app.get('/cart', (_req, res) => {
 			res.send('406 Not Acceptable');
 		},
 	});
+});
+
+app.get('/cart/:id', (req, res) => {
+	res.format({
+		'application/json': () => {
+			res.status(200);
+			res.type('application/json');
+			res.json(cart.find(({ id }) => id === parseInt(req.params.id)) || {});
+		},
+		default: () => {
+			res.status(406);
+			res.type('text/plain');
+			res.send('406 Not Acceptable');
+		},
+	});
+});
+
+app.put('/cart/:id', express.json(), (req, res) => {
+	console.log(req.body);
 });
 
 app.listen(PORT, () => {
