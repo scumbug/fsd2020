c4ff550910712090983a60cf8764eddf8aff595c
CS <chunsiang10@gmail.com>
2020-12-08 14:46:13 +0800

    added frontend query

---
+++ client/model.interface.ts
@@ -0,0 +1,9 @@
+export class Totals {
+  order_id: number;
+  date: Date;
+  customer_id: number;
+  total_quantity: number;
+  total_price: number;
+  total_discount: number;
+  total_cost: number;
+}
